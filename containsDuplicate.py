20080b10bb0b743906aa9ba5fb86b290e84d7366
Khánh Đức <khanhduc1022004@gmail.com>
2026-01-27 17:15:46 +0700

    Add Python algorithm solutions

---
+++ containsDuplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+        
+        for x in nums:
+            if x in seen:
+                return True  
+            seen.add(x)
+        return False  
